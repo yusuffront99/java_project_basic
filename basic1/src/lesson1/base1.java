8455beefc65e4e6d7c7f70f5539b613eedfcf4bd
yusuf <yusuffront@gmail.com>
2021-09-26 14:26:11 +0800

    base1 -- create new text uppercase and lowercase

---
+++ basic1/src/lesson1/base1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lesson1;
+
+/**
+ *
+ * @author myusu
+ */
+public class base1 {
+    public static void main(String[] args) {
+        
+        String name = "muhammad Yusuf";
+        String name1 = "HELLO JAVA";
+        
+        System.out.println(name.toUpperCase());
+        System.out.println(name1.toLowerCase());
+
+    }
+}
