12b4bf7ebc2755bd9b10982d8043706c49dd3515
yusuf <yusuffront@gmail.com>
2021-09-26 13:56:14 +0800

    exercise day 1

---
+++ basic1/src/lesson1/base.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lesson1;
+
+/**
+ *
+ * @author myusu
+ */
+public class base {
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+    }
+}
