1ba181bdee8d9a925a251e0f754b48992769b889
yusuf <yusuffront@gmail.com>
2021-09-27 09:05:21 +0800

    update base file java

---
+++ basic1/src/lesson1/base17.java
@@ -0,0 +1,11 @@
+package lesson1;
+
+public class base17 {
+    public static void main(String[] args) {
+        String[] cars = {"bmw","jazz","avanza"};
+
+        for(String i : cars){
+            System.out.println(i);
+        }
+    }
+}
---
+++ basic1/src/lesson1/base18.java
@@ -0,0 +1,14 @@
+package lesson1;
+
+public class base18 {
+    public static void main(String[] args) {
+        int x = 7;
+        for(int i = 0; i < 10; i++)
+        {
+            if(i == x){
+                break;
+            }
+            System.out.println(i);
+        }
+    }
+}
