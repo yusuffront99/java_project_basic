667785a7b2396ccd5fc67c2851150320e6efb090
yusuf <yusuffront@gmail.com>
2021-09-28 07:00:45 +0800

    update file java --inheritances

---
+++ basic1/src/Project/Formula.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Project;
+
+/**
+ *
+ * @author myusu
+ */
+public class Formula extends Attributes {
+    int hasil;
+    public int add(int x, int y){
+        return hasil = x + y; 
+    }
+}
---
+++ basic1/src/Project/Results.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Project;
+
+/**
+ *
+ * @author myusu
+ */
+public class Results extends Formula {
+    public static void main(String[] args) {
+        Results value = new Results();
+        value.add(5, 20);
+        System.out.println("Hasil penjumlahan adalah = "+value.hasil);
+    }
+}
