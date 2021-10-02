dcb5ad738c585b57b12a2d6fa73b7c6d3fc5b842
yusuf <yusuffront@gmail.com>
2021-10-02 20:11:42 +0800

    create new file java

---
+++ basic1/src/exercises/exercise15.java
@@ -0,0 +1,20 @@
+package exercises;
+
+public class exercise15 {
+    public static void main(String[] args) {
+        Integer myInt = 22;
+        Double myDouble = 45.77;
+        Character myChar = 'Z';
+
+        System.out.println(myInt);
+        System.out.println(myDouble);
+        System.out.println(myChar);
+
+        System.out.println("=========================");
+
+        // certain methods
+        System.out.println(myInt.intValue());
+        System.out.println(myDouble.doubleValue());
+        System.out.println(myChar.charValue());
+    }
+}
---
+++ basic1/src/exercises/exercise16.java
@@ -0,0 +1,9 @@
+package exercises;
+
+public class exercise16 {
+    public static void main(String[] args) {
+        Integer myInt = 12345;
+        String myString = myInt.toString();
+        System.out.println(myString.length());
+    }
+}
