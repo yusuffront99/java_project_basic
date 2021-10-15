190b01cc16b5b09b04c5c92162414f7110290736
yusuf <yusuffront@gmail.com>
2021-10-15 20:02:49 +0800

    create new label inputan

--- basic1/src/swing/lesson11.java
+++ basic1/src/swing/lesson11.java
@@ -1,20 +1,32 @@
 package swing;
 
-import javax.swing.JFrame;
-import javax.swing.JLabel;
+ import javax.swing.JFrame;
+ import javax.swing.JLabel;
+ import javax.swing.JTextField;
 
 public class lesson11 {
     public static void main(String[] args) {
         // container
         JFrame frame = new JFrame("Aplikasi Perpusatakaan");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400,500);
-        frame.setLayout(null);;
-        frame.setVisible(true);
 
         // ========= COMPONENTS ==========
         JLabel lbl = new JLabel("APLIKASI PERPUSTAKAAN");
         lbl.setBounds(110,30,180,30);
         frame.add(lbl);
+
+        JLabel labelKbuku = new JLabel("Kode Buku");
+        labelKbuku.setBounds(20,80,150,30);
+        frame.add(labelKbuku);
+
+        JTextField txtKBuku = new JTextField();
+        txtKBuku.setBounds(150, 80, 150, 30);
+        frame.add(txtKBuku);
+
+        frame.setSize(400,500);
+        frame.setLayout(null);
+        frame.setVisible(true);
     }
 }
+
+
