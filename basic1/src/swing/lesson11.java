6babac27adf50b0f0dd4ff4ed325ab04359dc4d2
yusuf <yusuffront@gmail.com>
2021-10-15 11:07:09 +0800

    update file contents

--- basic1/src/swing/lesson11.java
+++ basic1/src/swing/lesson11.java
@@ -1,13 +1,20 @@
 package swing;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class lesson11 {
     public static void main(String[] args) {
+        // container
         JFrame frame = new JFrame("Aplikasi Perpusatakaan");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400,600);
-        frame.setLocationRelativeTo(null);
+        frame.setSize(400,500);
+        frame.setLayout(null);;
         frame.setVisible(true);
+
+        // ========= COMPONENTS ==========
+        JLabel lbl = new JLabel("APLIKASI PERPUSTAKAAN");
+        lbl.setBounds(110,30,180,30);
+        frame.add(lbl);
     }
 }
