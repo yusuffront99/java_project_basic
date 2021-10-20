d29d67e5a1b5705d11547396295125839f26c3d7
yusuf <yusuffront@gmail.com>
2021-10-20 08:54:59 +0800

    create form number buttons

--- Students/src/Practices/Kalkulator.java
+++ Students/src/Practices/Kalkulator.java
@@ -7,6 +7,7 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.border.Border;
 
 public class Kalkulator extends JFrame {
     // Atts
@@ -20,33 +21,31 @@ public class Kalkulator extends JFrame {
         frame = new JFrame("Kalkulator");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         input = new JTextField();
+        input.setBounds(10,20,200,30);
         panel1 = new JPanel();
-        
+        panel2 = new JPanel();
 
-        // Components
         btn1 = new JButton("1"); btn4 = new JButton("4"); btn7 = new JButton("7");
         btn2 = new JButton("2"); btn5 = new JButton("5"); btn8 = new JButton("8");
         btn3 = new JButton("3"); btn6 = new JButton("6"); btn9 = new JButton("9");
         btn0 = new JButton("0"); btnDot = new JButton("."); btnCe = new JButton("CE");
-
-        // Field
-        panel2.add(input, BorderLayout.NORTH);
-        frame.add(panel1, BorderLayout.EAST);
         
+        panel1.add(input);
 
-        // // Sets Bounds
-        // frame.add(btn1);  frame.add(btn4);  frame.add(btn7);
-        // frame.add(btn2);  frame.add(btn5);  frame.add(btn8);
-        // frame.add(btn3);  frame.add(btn6);  frame.add(btn9);
-        // frame.add(btn0);  frame.add(btnDot);  frame.add(btnCe);
-        // frame.add(panel2);
+        panel2.add(btn4);
+        panel2.add(btn5);
+        panel2.add(btn6);
 
-        // Add
-        frame.setLayout(new GridLayout(3,3,20,25));
+        panel2.add(btn7);
+        panel2.add(btn8);
+        panel2.add(btn9);
 
-        frame.setSize(350,400);
+        frame.setLayout(new BorderLayout());
+        frame.add(panel1, BorderLayout.NORTH);
+        frame.add(panel2, BorderLayout.SOUTH);
+
+        frame.setSize(240,300);
         frame.setVisible(true);
-
     }
 
     public static void main(String[] args) {
---
+++ Students/src/Practices/Numbers.java
@@ -0,0 +1,52 @@
+package src.Practices;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+import java.awt.GridLayout;
+
+public class Numbers extends JFrame {
+    
+    // All Buttons
+    JButton b1 = new JButton("1");
+    JButton b2 = new JButton("2");
+    JButton b3 = new JButton("3");
+    JButton b4 = new JButton("4");
+    JButton b5 = new JButton("5");
+    JButton b6 = new JButton("6");
+    JButton b7 = new JButton("7");
+    JButton b8 = new JButton("8");
+    JButton b9 = new JButton("9");
+    JButton b0 = new JButton("0");
+    JButton bDot = new JButton(".");
+    JButton bCe = new JButton("CE");
+
+    JPanel panel;
+    JTextField inp;
+    GridLayout family;
+
+    public Numbers(){
+        super("Numbers");
+        setSize(260,260);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        panel = new JPanel();
+        family = new GridLayout(4,2);
+
+        panel.setLayout(family);
+        panel.add(b1); panel.add(b4); panel.add(b7);
+        panel.add(b2); panel.add(b5); panel.add(b8);
+        panel.add(b3); panel.add(b6); panel.add(b9);
+        panel.add(b0); panel.add(bDot); panel.add(bCe);
+
+        add(panel);
+        setVisible(true);
+    }
+
+        public static void main(String[] args) {
+            new Numbers();
+        }
+ 
+}
