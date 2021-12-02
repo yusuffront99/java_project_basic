d1b34e388beeebdf13c025bd9655882ff66b29cb
yusuf <yusuffront@gmail.com>
2021-12-02 22:52:07 +0800

    create more projects

---
+++ PertambahanAngka/src/ApkPertambahanAngka/Filter.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ApkPertambahanAngka;
+
+import javax.swing.JOptionPane;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author myusu
+ */
+public class Filter {
+    public static void digitFilter(KeyEvent evt){
+        char inp = evt.getKeyChar();
+        if (! ((Character.isDigit(inp)) || (inp == KeyEvent.VK_BACK_SPACE) || (inp == KeyEvent.VK_DELETE))){
+            new JFrame().getToolkit().beep();
+            JOptionPane.showMessageDialog(null, "Masukkan nilai 0 - 9");
+            evt.consume();
+        }
+    }
+}
